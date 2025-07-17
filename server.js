8d53b791b30363199a71750f0bdf4e03e8e3690e
RLGADM <romain.rlg.adm@gmail.com>
2025-07-17 03:21:32 +0200

    maj bug server

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const io = new Server(server, {
     origin: [
       "http://localhost:5173",
       "https://localhost:5173",
-      "https://kenshou.netlify.app/",
+      "https://kenshou.netlify.app",
       "https://kensho-hab0.onrender.com"
     ],
     methods: ["GET", "POST"],
