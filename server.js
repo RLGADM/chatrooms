9336499e6e900ebb9122ee068c55507ff7a597f7
RLGADM <romain.rlg.adm@gmail.com>
2025-07-17 03:16:28 +0200

    Mise à jour de la configuration du serveur Socket

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+//Bonjour
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
