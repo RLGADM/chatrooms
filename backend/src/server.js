36fa1e61b0f0b8cc759300106f525c478c1632ac
rlgadm <romain.rlg.adm@gmail.com>
2025-08-28 19:48:13 +0200

    chépa

--- backend/src/server.js
+++ backend/src/server.js
@@ -46,7 +46,7 @@ console.log('[SERVER] Maps rooms et users vidées au démarrage');
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: 'https://localhost:5173',
     credentials: true,
   },
 });
